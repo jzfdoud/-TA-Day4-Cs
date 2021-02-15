6e5b918d5773e1b48b01785865f5866d538525ce
Jada <jada.fagan@gmail.com>
2021-02-15 10:08:26 -0500

    Add project files.

---
+++ (TA)Day4-Cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _TA_Day4_Cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var sum = 0;
+            for (var idx = 1; idx <= 70; idx++)
+            {
+                if (idx % 7 == 0)
+                {
+                    sum += idx;
+                }
+            }
+            Console.WriteLine($"total of nbrs divisible by 7: {sum}");
+        }
+    }
+}
